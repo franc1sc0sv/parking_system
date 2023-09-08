020955da9bc1aa5bb8cb5a251305c912e6861376
Francisco Josue <francisco.sv.xd@gmail.com>
2023-09-07 19:00:49 -0600

    asexo

--- src/pages/ParkingSlot/Components/ParkingContainer.jsx
+++ src/pages/ParkingSlot/Components/ParkingContainer.jsx
@@ -1,3 +1,6 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
 const getParqueos = (jsonData) => {
   const parqueosReservables = [];
   const parqueosNoReservables = [];
@@ -17,13 +20,39 @@ const ParkingContainer = ({ parqueos, handleReservar }) => {
     const estaReservado = parqueo.reservas.some(
       (reserva) => reserva.estado === "activo"
     );
-    return { ...parqueo, estaReservado };
+    return { ...parqueo, estaReservado, isAvailable: true };
   });
-  const { parqueosReservables, parqueosNoReservables } =
-    getParqueos(formatedParqueos);
+
+  const [parqueosReservables, setParqueos] = useState(() => {
+    const { parqueosReservables } = getParqueos(formatedParqueos);
+    return parqueosReservables;
+  });
+
+  const [parqueosNoReservables, setNoParqueos] = useState(() => {
+    const { parqueosNoReservables } = getParqueos(formatedParqueos);
+    return parqueosNoReservables;
+  });
 
   const noParqueosReservables = parqueosReservables.length;
   const noParqueosNoReservables = parqueosNoReservables.length;
+
+  const handleOnChangeNoParqueo = (id) => {
+    const formatedData = parqueosNoReservables.map((parqueo) => {
+      return parqueo.id === id
+        ? { ...parqueo, isAvailable: !parqueo.isAvailable }
+        : { ...parqueo };
+    });
+    setNoParqueos(formatedData);
+  };
+
+  const handleOnChangeParqueo = (id) => {
+    const formatedData = parqueosReservables.map((parqueo) => {
+      return parqueo.id === id
+        ? { ...parqueo, isAvailable: !parqueo.isAvailable }
+        : { ...parqueo };
+    });
+    setParqueos(formatedData);
+  };
 
   return (
     <div className="flex gap-[150px] w-max mx-auto mt-10  border-[#40454c] rounded-md">
@@ -32,9 +61,10 @@ const ParkingContainer = ({ parqueos, handleReservar }) => {
           <ParkingSlot
             handleReservar={handleReservar}
             key={parqueo.numeroParqueo}
-            isAvailable={true}
+            isAvailable={parqueo.isAvailable}
             parqueo={parqueo}
             isTheLast={i === noParqueosReservables - 1}
+            handleOnChangeParqueo={handleOnChangeParqueo}
           />
         ))}
       </div>
@@ -46,8 +76,9 @@ const ParkingContainer = ({ parqueos, handleReservar }) => {
           <ParkingSlotReverse
             handleReservar={handleReservar}
             key={parqueo.numeroParqueo}
-            isAvailable={true}
+            isAvailable={parqueo.isAvailable}
             parqueo={parqueo}
+            handleOnChangeNoParqueo={handleOnChangeNoParqueo}
             isTheLast={i === noParqueosNoReservables - 1}
           />
         ))}
@@ -56,32 +87,56 @@ const ParkingContainer = ({ parqueos, handleReservar }) => {
   );
 };
 
-const ParkingSlot = ({ isTheLast, isAvailable, parqueo, handleReservar }) => {
+const ParkingSlot = ({
+  isTheLast,
+  isAvailable,
+  parqueo,
+  handleReservar,
+  handleOnChangeParqueo,
+}) => {
   const { numeroParqueo, estaReservado } = parqueo;
   const boderB = isTheLast ? "border-b-4" : "";
-
+  const initialAnimation = { opacity: 0 };
+  const animateAnimation = { opacity: 1 };
+  const exitAnimation = { opacity: 0 };
+  const transitionProps = { duration: 0.2 };
   return (
     <div
-      onClick={() => {
-        if (!estaReservado) {
-          handleReservar(parqueo.id, parqueo.sePuedeReservar);
-        }
-      }}
       className={`relative cursor-pointer justify-end flex w-56 border-[#40454c] h-28 border-t-4  ${boderB}`}
     >
       <div className="w-10 border-r-4 border-[#40454c] h-auto "></div>
-
-      {!isAvailable ? (
-        <div className="grid w-full place-items-center">
-          <img src="/public/car.svg" alt="nose" className="w-[125px] " />
-        </div>
-      ) : (
-        <div className="grid w-full place-items-center">
-          <p className="text-[#a2a4ab] text-lg">
-            {estaReservado ? "Reservado" : "Disponible"}
-          </p>
-        </div>
-      )}
+      <input
+        class="relative float-right -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+        type="checkbox"
+        checked={!isAvailable}
+        value={!isAvailable}
+        onChange={() => {
+          handleOnChangeParqueo(parqueo.id);
+        }}
+      />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={!isAvailable}
+          initial={initialAnimation}
+          animate={animateAnimation}
+          exit={exitAnimation}
+          transition={transitionProps}
+          className="grid w-full place-items-center"
+          onClick={() => {
+            if (!estaReservado) {
+              handleReservar(parqueo.id, parqueo.sePuedeReservar);
+            }
+          }}
+        >
+          {!isAvailable ? (
+            <img src="/public/car.svg" alt="nose" className="w-[125px] " />
+          ) : (
+            <p className="text-[#a2a4ab] text-lg">
+              {estaReservado ? "Reservado" : "Disponible"}
+            </p>
+          )}
+        </motion.div>
+      </AnimatePresence>
 
       <p className="text-[#a2a4ab] font-bold text-lg absolute bottom-0 right-0 ">
         {numeroParqueo}
@@ -95,35 +150,57 @@ const ParkingSlotReverse = ({
   isAvailable,
   parqueo,
   handleReservar,
+  handleOnChangeNoParqueo = () => {},
 }) => {
   const { numeroParqueo, estaReservado } = parqueo;
 
   const boderB = isTheLast ? "border-b-4" : "";
+  const initialAnimation = { opacity: 0 };
+  const animateAnimation = { opacity: 1 };
+  const exitAnimation = { opacity: 0 };
+  const transitionProps = { duration: 0.2 };
   return (
     <div
-      onClick={() => {
-        if (!estaReservado) {
-          handleReservar(parqueo.id, parqueo.sePuedeReservar);
-        }
-      }}
       className={`relative cursor-pointer justify-end flex w-56 border-[#40454c] h-28 border-t-4  ${boderB}`}
     >
-      {!isAvailable ? (
-        <div className="grid w-full place-items-center">
-          <img src="/public/car.svg" alt="nose" className="w-[125px] " />
-        </div>
-      ) : (
-        <div className="grid w-full place-items-center">
-          <p className="text-[#a2a4ab] text-lg">
-            {estaReservado ? "Reservado" : "Disponible"}
-          </p>
-        </div>
-      )}
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={!isAvailable}
+          initial={initialAnimation}
+          animate={animateAnimation}
+          exit={exitAnimation}
+          transition={transitionProps}
+          className="grid w-full place-items-center"
+          onClick={() => {
+            if (!estaReservado) {
+              handleReservar(parqueo.id, parqueo.sePuedeReservar);
+            }
+          }}
+        >
+          {!isAvailable ? (
+            <img src="/public/car.svg" alt="nose" className="w-[125px] " />
+          ) : (
+            <p className="text-[#a2a4ab] text-lg">
+              {estaReservado ? "Reservado" : "Disponible"}
+            </p>
+          )}
+        </motion.div>
+      </AnimatePresence>
 
       <p className="text-[#a2a4ab] font-bold text-lg absolute bottom-0 left-0 ">
         {numeroParqueo}
       </p>
       <div className="w-10 border-l-4 border-[#40454c] h-auto "></div>
+
+      <input
+        class="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+        type="checkbox"
+        checked={!isAvailable}
+        value={!isAvailable}
+        onChange={() => {
+          handleOnChangeNoParqueo(parqueo.id);
+        }}
+      />
     </div>
   );
 };
